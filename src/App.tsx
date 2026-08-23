dd613ba7a8509cd223701d0504dc7946596117d4
Daniel Aparisi <daniel.aparisi.lozano@gmail.com>
2026-08-23 22:23:45 +0200

    feat: doing singUp function from suoabase and create a table called admins on supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from 'react'
+import { supabase } from './lib/supabaseClient'
+
+type Admin = {
+  id: number
+  created_at: string
+  name: string
+}
+
 function App() {
+  const [admins, setAdmins] = useState<Admin[]>([])
+
+  useEffect(() => {
+    getAdmins()
+  }, [])
+
+  async function getAdmins() {
+    const { data, error } = await supabase.from('Admins').select()
+
+    if (error) {
+      console.error(error)
+      return
+    }
+
+    setAdmins(data)
+  }
+
   return (
-    <>
-      <h1>Hola</h1>
-    </>
+    <div>
+      <h1>Admins</h1>
+      {admins.length === 0 ? (
+        <p>No hay admins todavía.</p>
+      ) : (
+        <ul>
+          {admins.map((admin) => (
+            <li key={admin.id}>{admin.name}</li>
+          ))}
+        </ul>
+      )}
+    </div>
   )
 }
 
